7905dc334305f2340bcabe022fae7406d201c250
Henry Chan <henry@kinwo.net>
2023-12-29 15:12:25 +1000

    feat(queryController) add isTesting to params

--- app/client/user.ts
+++ app/client/user.ts
@@ -6,6 +6,7 @@ import { log } from '../util/log';
 const API_KEY: string = process.env.NEXT_PUBLIC_API_KEY ?? '';
 const QUERY_CONTROLLER_URL: string =
   process.env.NEXT_PUBLIC_QUERY_CONTROLLER_URL ?? '';
+const IS_TESTING: boolean = process.env.NEXT_PUBLIC_IS_TESTING === 'true';
 
 export const queryUserController = async (
   identity: Identity
@@ -17,6 +18,7 @@ export const queryUserController = async (
       params: {
         apiKey: API_KEY,
         owner: principalId,
+        isTesting: IS_TESTING,
       },
     });
 
